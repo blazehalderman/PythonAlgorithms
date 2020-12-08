2cb2a9cbd60ffd42c377629a1717d9f8e666f64b
blazehalderman <blazerboy123@gmail.com>
2020-12-07 18:11:30 -0800

    bubble sort algorithm

--- BubbleSort/bubblesort.py
+++ BubbleSort/bubblesort.py
@@ -1,10 +1,22 @@
-#Initial idea for bubble sort(sort once)
+"""
+
+  Write a function that takes in an array of integers and returns a sorted
+  version of that array. Use the Bubble Sort algorithm to sort the array.
+
+"""
+#Updated and working idea for bubble sort
 
 def bubbleSort(array):
     # Write your code here.
-	for i in range(len(array) - 1):
-		if (array[i] > array[i + 1]):
-			shift = array[i + 1]
-			array[i + 1] = array[i]
-			array[i] = shift
+	count = 0
+	sort = False
+	while not sort:
+		sort = True
+		for i in range(len(array) - 1 - count):
+			if (array[i] > array[i + 1]):
+				shift = array[i + 1]
+				array[i + 1] = array[i]
+				array[i] = shift
+				sort = False
+		count += 1
 	return array
